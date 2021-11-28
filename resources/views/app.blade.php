8c16f5b619f6c79d0bc62789e413401730781f4d
Pablo Giraldo <pablogirald@yahoo.es>
2021-11-28 18:56:37 +0100

    Change navbar

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -19,12 +19,15 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-            <a class="navbar-brand" href="{{ route('todos.index') }}">Tareas</a>
+            <a class="navbar-brand" href="{{ route('todos.index') }}">Inicio</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
                 aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link" href="{{ route('todos.index') }}">Tareas</a>
+                </div>
                 <div class="navbar-nav">
                     <a class="nav-link" href="{{ route('categories.index') }}">Categorías</a>
                 </div>
