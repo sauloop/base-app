d80c36d4b66dceba6bb442ef0abbbb4d74bdf734
Pablo Giraldo <pablogirald@yahoo.es>
2021-12-02 15:11:51 +0100

    Change do space config

--- config/filesystems.php
+++ config/filesystems.php
@@ -62,6 +62,17 @@ return [
             'endpoint' => env('DO_SPACES_ENDPOINT'),
         ],
 
+        'spaces' => [
+            'driver' => 's3',
+            'key' => env('DO_SPACES_ACCESS_KEY_ID'),
+            'secret' => env('DO_SPACES_SECRET_ACCESS_KEY'),
+            'region' => env('DO_SPACES_DEFAULT_REGION'),
+            'bucket' => env('DO_SPACES_BUCKET'),
+            'endpoint' => env('DO_SPACES_ENDPOINT'),
+            'bucket_endpoint' => true,
+            'visibility' => 'public',
+          ],
+
     ],
 
     /*
